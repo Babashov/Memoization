acba77624d713ee0c7d8ffa2b017a52de2434695
Babashov Mahammad <babashov.info@gmail.com>
2023-01-20 07:50:22 +0400

    Object props is not working React.memo()

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { useState } from 'react';
 
 function App() {
   const [number,setNumber] = useState(0)
+  const user = {name:'Babashov'}
+
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number}/>
+      <Header number={number < 5 ? 0 : number} user={user}/>
       <hr/>
       <h1>{number}</h1>
       <button onClick={()=>setNumber(number+1)}>Click</button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 
-function Header({number}) {
+function Header({number,user}) {
   return (
     <div>
         <h1>Header - {number}</h1>
+        <br/><br/>
+        {JSON.stringify(user)}
         {console.log('Header Component')}
     </div>
   )
