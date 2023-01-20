57bfe8accf2504970a38c3f907b5a281c6abbf42
Babashov Mahammad <babashov.info@gmail.com>
2023-01-20 08:29:29 +0400

    Expensive Function & useMemo difference

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import { useState,useMemo } from 'react';
 
 function App() {
   const [number,setNumber] = useState(0)
+  const [text,setText] = useState('')
+
+
+  // Calculating only changing number
   const user = useMemo(()=>{
+    calculate()
     return {name:'Babashov'}
-  },[])
+  },[number])
+
+  // Expensive Function
+  // const user = calculate();
 
   return (
     <div className="App">
@@ -14,8 +22,23 @@ function App() {
       <hr/>
       <h1>{number}</h1>
       <button onClick={()=>setNumber(number+1)}>Click</button>
+
+      <br/><br/>
+
+      <input value={text} onChange={({target})=>setText(target.value)} />
     </div>
   );
+}
+
+function calculate()
+{
+  console.log('Calculate...');
+
+  for (let index = 0; index < 1000000000; index++) {
+    
+  }
+
+  console.log("Calculate end!");
 }
 
 export default App;
